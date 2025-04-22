278c8f6fbd577c218a97e9127c48f6f76bcea503
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-04-22 14:19:52 +0000

    [feat] distringuish local run from deployed link (for evaluation)

--- src/constants/api_url.py
+++ src/constants/api_url.py
@@ -1,3 +1,4 @@
 """Global variable - used in app and src/evaluate.py"""
 
-API_BASE_URL = "http://localhost:5000"  # or wherever your FastAPI is running
+API_LOCAL_URL = "http://localhost:5000"  # or wherever your FastAPI is running
+API_DEPLOYED_URL = "https://codification-ape-graph-rag.lab.sspcloud.fr/api"
