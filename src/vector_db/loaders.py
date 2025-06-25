51913d6aa034d8c1d6e0014548e60257a4ac17c2
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-06-25 14:49:55 +0000

    fix : clean database before recreating it

--- src/vector_db/loaders.py
+++ src/vector_db/loaders.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from langchain_neo4j import Neo4jGraph, Neo4jVector
 from langchain_openai import OpenAIEmbeddings
+from neo4j import GraphDatabase
 
 # from vector_db.openai_embeddings import CustomOpenAIEmbeddings
 from constants.graph_db import NEO4J_PWD, NEO4J_URL, NEO4J_USERNAME
@@ -21,8 +22,28 @@ if EMBEDDING_MODEL is None:
 logger = logging.getLogger(__name__)
 
 
-def create_vector_db(docs, embedding_model) -> Neo4jVector:
+def execute_cypher_command(query, parameters=None):
+    driver = GraphDatabase.driver(NEO4J_URL, auth=(NEO4J_USERNAME, NEO4J_PWD))
+
+    try:
+        with driver.session() as session:
+            result = session.run(query, parameters or {})
+            return result.data()  # Returns list of records
+    except Exception as e:
+        logging.error(f"Error executing Cypher query: {e}")
+        raise
+    finally:
+        driver.close()
+
+
+def create_vector_db(docs, embedding_model, clean_previous: bool = True) -> Neo4jVector:
     logger.info("🧠 Creating Neo4j vector DB with embeddings")
+
+    if clean_previous:
+        command = "DROP INDEX vector IF EXISTS"
+        logger.info("🧹 Cleaning previous vector DB. Running command " + command)
+        execute_cypher_command(command)
+
     return Neo4jVector.from_documents(
         docs, embedding_model, url=NEO4J_URL, username=NEO4J_USERNAME, password=NEO4J_PWD, ids=[f"{i}" for i in range(len(docs))]
     )
